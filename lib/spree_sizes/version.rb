7ed5ead0d5b4fe67fd019d5e050d0c14fca47cd6
Raymond Usbal <raymond@philippinedev.com>
2018-10-03 16:12:19 +0800

    Up version

--- lib/spree_sizes/version.rb
+++ lib/spree_sizes/version.rb
@@ -10,7 +10,7 @@ module SpreeSizes
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 103
+    TINY  = 104
     PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
